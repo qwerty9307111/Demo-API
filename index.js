a28721cde3ce1d5812053fb8f61ed5714de6338a
qwerty9307111 <2436415068@qq.com>
2021-03-25 17:35:08 +0800

    commit

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express();
 const port = 9999
 
-var testRouter = require('./request');
-app.use('/api', testRouter);
-
 app.use('/static', express.static(path.join(__dirname, 'docs')));
 
 app.listen(port, () => console.log('http://localhost:' + port + '/static/index.html'));
